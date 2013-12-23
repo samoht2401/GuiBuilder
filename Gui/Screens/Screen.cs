92e61665070ce05c6ab6db58e554c9a591a4f36d
samoht2401 <thomasbauvent@gmail.com>
2013-12-23 13:35:39 +0100

    Add Screen

---
+++ Gui/Screens/Screen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gui.Screens
+{
+    public abstract class Screen
+    {
+        //Transition part
+        public int Transition
+    }
+}
